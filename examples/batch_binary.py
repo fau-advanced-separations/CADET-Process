d61821c07d98126a3998b2d75b3b01529cb83e86
Johannes Schmölder <johannes.schmoelder@fau.de>
2019-12-17 11:09:06 +0100

    first examples

---
+++ examples/batch_binary.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+from CADETProcess.processModel import Langmuir
+from CADETProcess.processModel import Source, Column, Sink
+from CADETProcess.processModel import FlowSheet
+from CADETProcess.processModel import Process
+
+process_name = flow_sheet_name = 'batch_binary'
+
+# Binding Model
+binding_model = Langmuir(n_comp=2, name='langmuir')
+binding_model.adsorption_rate = [0.02, 0.03]
+binding_model.desorption_rate = [1, 1]
+binding_model.saturation_capacity = [100, 100]
+
+# Unit Operations
+feed = Source(n_comp=2, name='feed')
+feed.c = [10, 10]
+
+eluent = Source(n_comp=2, name='eluent')
+eluent.c = [0, 0]
+
+column = Column(n_comp=2, name='column')
+column.length = 0.6
+column.diameter = 0.024
+column.axial_dispersion = 4.7e-7
+column.total_porosity = 0.7
+
+column.binding_model = binding_model
+
+outlet = Sink(n_comp=2, name='outlet')
+
+# flow sheet
+fs = FlowSheet(n_comp=2, name=flow_sheet_name)
+
+fs.add_unit(feed, feed_source=True)
+fs.add_unit(eluent, eluent_source=True)
+fs.add_unit(column)
+fs.add_unit(outlet, chromatogram_sink=True)
+
+fs.add_connection(feed, column)
+fs.add_connection(eluent, column)
+fs.add_connection(column, outlet)
+
+# Process
+batch_binary = Process(fs, name=process_name)
+
+Q = 60/(60*1e6)
+
+# Create Events and Durations
+batch_binary.add_event('feed_on', 'flow_sheet.feed.flow_rate', Q)
+batch_binary.add_event('feed_off', 'flow_sheet.feed.flow_rate', 0.0)
+batch_binary.add_duration('feed_duration', 'feed_on', 'feed_off')
+
+batch_binary.add_event('eluent_on', 'flow_sheet.eluent.flow_rate', Q)
+batch_binary.add_event('eluent_off', 'flow_sheet.eluent.flow_rate', 0.0)
+
+batch_binary.add_event_dependency('eluent_on', ['feed_off'])
+batch_binary.add_event_dependency('eluent_off', ['feed_on'])
+batch_binary.add_event_dependency('feed_off', ['feed_on', 'feed_duration'],[1,1])
+
+# Set process times
+batch_binary.cycle_time = 174.7
+batch_binary.feed_duration.time = 51.2
+
+
+if __name__ == '__main__':
+    from CADETProcess.simulation import Cadet
+    process_simulator = Cadet()
+    process_simulator.evaluate_stationarity = False
+    process_simulator.n_cycles = 4
+
+    batch_binary_sim_results = process_simulator.simulate(batch_binary)
+
+    from CADETProcess.fractionation import optimize_fractionation
+    batch_binary_performance = optimize_fractionation(
+            batch_binary_sim_results.chromatograms,
+            batch_binary.process_meta,
+            0.95)
---
+++ examples/batch_binary_optimization.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from CADETProcess.simulation import Cadet, ProcessEvaluator
+from CADETProcess.optimization import OptimizationProblem
+from CADETProcess.common import RankedPerformance
+
+from examples.batch_binary import batch_binary
+
+process_simulator = Cadet()
+process_simulator.evaluate_stationarity = True
+
+ranking = [1, 1]
+purity_required = [0.95, 0.95]
+evaluator = ProcessEvaluator(process_simulator, purity_required, ranking)
+
+optimization_problem = OptimizationProblem(batch_binary, evaluator, save_log=True)
+optimization_problem.cache_results = True
+
+def objective_function(performance):
+    performance = RankedPerformance(performance, ranking)
+    return - performance.mass * performance.recovery * performance.eluent_consumption
+
+optimization_problem.objective_fun = objective_function
+
+optimization_problem.add_variable('cycle_time', lb=10, ub=600)
+optimization_problem.add_variable('feed_duration.time', lb=10, ub=300)
+
+optimization_problem.add_linear_constraint(['feed_duration.time', 'cycle_time'], [1,-1])
+
+if __name__ == "__main__":
+    from CADETProcess.optimization import DEAP as Solver
+
+    opt_solver = Solver()
+    results = opt_solver.optimize(optimization_problem, save_results=True)
