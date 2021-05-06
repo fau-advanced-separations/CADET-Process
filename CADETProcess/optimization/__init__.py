d4729c231ff66175bc9d0019615e58fb738e03d0
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2021-05-06 11:48:38 +0200

    removed GPyOpt which is no longer maintained

--- CADETProcess/optimization/__init__.py
+++ CADETProcess/optimization/__init__.py
@@ -4,4 +4,3 @@ from .optimizationProblem import *
 from .solver import *
 from .scipyAdapter import COBYLA, TrustConstr, NelderMead, SLSQP
 from .deapAdapter import DEAP
-from .gpyoptAdapter import GPyOpt
