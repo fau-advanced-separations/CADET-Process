042929ddc036962002725db5dd8be2db3cded729
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2021-07-08 18:11:17 +0200

    make timeline callable for arrays

--- CADETProcess/common/section.py
+++ CADETProcess/common/section.py
@@ -1,4 +1,6 @@
-from numpy.polynomial import Polynomial
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
 
 from CADETProcess import CADETProcessError
 
@@ -13,20 +15,34 @@ class Section():
         End time of section
     state : float or list of floats
         StateTuple of polynomial coefficients in order of increasing degree.
-    poly : bool
-        If True, the states are assumed to be polynomial coefficients.
     """
-    def __init__(self, start, end, state, poly=False):
-        if poly:
-            self._poly = Polynomial(state, domain=(start, end), window=(0,1))
+    def __init__(self, start, end, state):
+        if isinstance(state, bool):
+            state = int(state)
+            
+        if isinstance(state, (int, float)):
+            state = (state,)
+            
+        if not isinstance(state, tuple):
+            raise TypeError('Expected tuple')
+        
+        if len(state) <= 4:
+            missing = 4 - len(state)
+            state = state + missing*(0,)
         else:
-            self._poly = None
+            raise CADETProcessError('Only cubic polynomials are supported')
 
-        self.state = state
+        self._poly = np.polynomial.Polynomial(
+            state, domain=(start, end), window=(0,1)
+        )
 
         self.start = start
         self.end = end
 
+    @property
+    def state(self):
+        return self._poly.coef
+    
     def value(self, t):
         """Return value of function at time t.
 
@@ -45,13 +61,11 @@ class Section():
         ValueError
             If t is lower than start or higher than end of section times.
         """
-        if not (self.start <= t <= self.end):
-            raise ValueError('Exceeds bounds')
+        if np.any(t < self.start) or np.any(self.end < t):
+            raise ValueError('Time exceeds section times')
 
-        if self._poly is not None:
-            return self._poly(t)
+        return self._poly(t)
 
-        return self.state
 
     def integral(self, start=None, end=None):
         """Return integral of function in interval [start, end].
@@ -77,15 +91,12 @@ class Section():
             start = self.start
         if end is None:
             end = self.end
-
-        if not (self.start <= start < end <= self.end):
+            
+        if not (self.start <= start) & (start < end) & (end < self.end):
             raise ValueError('Integration bounds exceed section times')
 
-        if self._poly is not None:
-            integ = self._poly.integ(lbnd=start)
-            return integ(end)
-        else:
-            return (end-start) * self.state
+        integ = self._poly.integ(lbnd=start)
+        return integ(end)
 
 class TimeLine():
     def __init__(self):
@@ -94,24 +105,24 @@ class TimeLine():
     @property
     def sections(self):
         return self._sections
-
+    
     def add_section(self, section):
         if not isinstance(section, Section):
             raise CADETProcessError('Expected Section')
 
         self._sections.append(section)
 
-    def value(self, t):
-        for index, section in enumerate(self.sections):
-
-            if index < len(self.sections)-1:
-                cond = section.start <= t < section.end
-            if index == len(self.sections)-1:
-                cond = section.start <= t <= section.end
-            if cond:
-                return section.value(t)
-
-        raise CADETProcessError('Could not find section')
+    def value(self, time):
+        x = [0.0]
+        coeffs = []
+        
+        for sec in self.sections:
+            coeffs.append(np.flip(sec.state))
+            x.append(sec.end)
+        
+        piecewise_poly = scipy.interpolate.PPoly(np.array(coeffs).T, x)
+        
+        return piecewise_poly(time)
 
     def integral(self, start, end):
         """Return integral of sections in interval [start, end].
@@ -137,10 +148,15 @@ class TimeLine():
             start = self.start
         if end is None:
             end = self.end
-
-        if not (self.start <= start < end <= self.end):
+                
+        if not (self.start <= start) & (start < end) & (end < self.end):
             raise ValueError('Integration bounds exceed section times')
+    
 
     @property
     def section_times(self):
             return [self.sections[0].start] + [sec.end for sec in self.sections]
+        
+    def plot(self):
+        plt.plot(self.time, self.value(self.time))
+
