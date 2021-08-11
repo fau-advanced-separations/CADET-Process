0431c56311303a2ff9bef1ce462091bd34cc99c6
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2021-08-11 21:56:55 +0200

    improved handling of polynomials and number of entries for sections

--- CADETProcess/common/section.py
+++ CADETProcess/common/section.py
@@ -3,98 +3,10 @@ import scipy
 import matplotlib.pyplot as plt
 
 from CADETProcess import CADETProcessError
+from CADETProcess.common import StructMeta, NdPolynomial
 
-class Section():
-    """Helper class to store parameter state between to sections.
-
-    Attributes
-    ----------
-    start : float
-        Start time of section
-    end : float
-        End time of section.
-    state : float or array_like
-        Parameter state during section
-    """
-    def __init__(self, start, end, state):
-        if isinstance(state, bool):
-            state = int(state)
-
-        if isinstance(state, (int, float, tuple, list)):
-            state = np.array((state,)).T
-            
-        self._state = state
-
-        self.start = start
-        self.end = end
-
-    @property
-    def state(self):
-        """
-        Each row represents the polynomial coefficients of one component
-        in increasing order.
-        """
-        return self._state
-    
-    @property
-    def n_dim(self):
-        return self.state.shape[0]
-    
-    def value(self, t):
-        """Return value of function at time t.
-
-        Parameters
-        ----------
-        t : float
-            Time at which function is evaluated.
-
-        Returns
-        -------
-        y : float
-            Value of attribute at time t.
-
-        Raises
-        ------
-        ValueError
-            If t is lower than start or larger than end of section time.
-        """
-        if np.any(t < self.start) or np.any(self.end < t):
-            raise ValueError('Time exceeds section times')
-            
-        return self.state
-    
-    def integral(self, start=None, end=None):
-        """Return integral of function in interval [start, end].
-
-        Parameters
-        ----------
-        start : float, optional
-            Lower integration bound.
-        end : float, optional
-            Upper integration bound.
-
-        Returns
-        -------
-        Y : float
-            Value of definite integral between start and end.
-
-        Raises
-        ------
-        ValueError
-            If integration bounds exceed section times.
-        """
-        if start is None:
-            start = self.start
-        if end is None:
-            end = self.end
-            
-        if not ((self.start <= start) & (start < end) & (end <= self.end)):
-            raise ValueError('Integration bounds exceed section times')
-
-        return (end - start) * self.state
-
-class PolynomialSection(Section):
-    """Helper class to store parameter state between to sections.
+class Section(metaclass=StructMeta):
+    """Helper class to store parameter states between events.
 
     Attributes
     ----------
@@ -102,56 +14,51 @@ class PolynomialSection(Section):
         Start time of section
     end : float
         End time of section.
-    state : float or array_like
+    state : int or float or array_like
         Polynomial coefficients of state in order of increasing degree.
+    n_entries : int
+        Number of entries (e.g. components, output_states)
     degree : int
         Degree of polynomial to represent state.
-    n_dim : int, optional
-        Number of state dimensions.
-        If not given, it will be inferred from the coeffs dimensions.
-    """
-    def __init__(self, start, end, state):
-        if isinstance(state, bool):
-            state = int(state)
-
-        if isinstance(state, (int, float)):
-            state = np.array((state), ndmin=2)
-                
-        if isinstance(state, (tuple, list)):
-            for s in state:
-                if isinstance(s, (tuple, list)):
-                    missing = 4 - len(s)
-                    s += missing*(0,)
-            state = np.array((state), ndmin=2)
-
-        if state.shape[1] > 4:
-            raise CADETProcessError('Only cubic polynomials are supported')
         
-        _state = np.zeros((state.shape[0], 4))
-        _state[:,0:state.shape[1]] = state
+    Notes
+    -----
+    if state is int: Set constant value for for all entries
+    if state is list: Set value per component (check length!)
+    if state is ndarray (or list of lists): set polynomial coefficients
+    """
+    state = NdPolynomial(dep=('n_entries', 'n_poly_coeffs'), default=0)
+    
+    def __init__(self, start, end, state, n_entries=None, degree=0):
+        if n_entries is None:
+            if isinstance(state, (int, bool, float)):
+                n_entries = 1
+            elif isinstance(state, (list, tuple, np.ndarray)) and degree == 0:
+                n_entries = len(state)
+            else:
+                raise ValueError("Ambiguous entries for n_entries and degree")
+        self.n_entries = n_entries
+        
+        if n_entries == 1:
+            state = [state]
+            
+        self.degree = degree
+        self.state = state
 
         self._poly = []
-        for s in _state:
+        for i in range(self.n_entries):
             poly = np.polynomial.Polynomial(
-                s, domain=(start, end), window=(0,1)
-            )   
+                self.state[i], domain=(start, end), window=(0,1)
+            )
             self._poly.append(poly)
 
         self.start = start
         self.end = end
 
     @property
-    def state(self):
-        """
-        Each row represents the polynomial coefficients of one component
-        in increasing order.
-        """
-        return np.array([p.coef for p in self._poly])
-    
-    @property
-    def n_dim(self):
-        return len(self._poly)
-    
+    def n_poly_coeffs(self):
+        return self.degree + 1
+
     def value(self, t):
         """Return value of function at time t.
 
@@ -208,13 +115,24 @@ class PolynomialSection(Section):
         integ_methods = [p.integ(lbnd=start) for p in self._poly]
         return np.array([i(end) for i in integ_methods])
 
+
 class TimeLine():
     def __init__(self):
-        self._sections = []
-
+        self._sections = [] 
+    
     @property
     def sections(self):
         return sorted(self._sections, key=lambda sec: sec.start)
+
+    @property
+    def degree(self):
+        if len(self.sections) > 0:
+            return self.sections[0].degree
+    
+    @property
+    def n_entries(self):
+        if len(self.sections) > 0:
+            return self.sections[0].n_entries
     
     def add_section(self, section):
         """Add section to TimeLine.
@@ -229,20 +147,42 @@ class TimeLine():
         TypeError
             If section is not instance of Section.
         CADETProcessError
-            If section dimensions do not match Timeline dimensions.
+            If polynomial degree does not match.
         CADETProcessError
             If section introduces a gap.
         """
         if not isinstance(section, Section):
             raise TypeError('Expected Section')
         if len(self.sections) > 0:
-            if section.n_dim != self.n_dim:
-                raise CADETProcessError('Number of dimensions does not match')
+            if section.degree != self.degree:
+                raise CADETProcessError('Polynomial degree does not match')
+
             if not (section.start == self.end or section.end == self.start):
                 raise CADETProcessError('Sections times must be without gaps')
 
         self._sections.append(section)
     
+        self.update_piecewise_poly()
+
+    def update_piecewise_poly(self):
+        x = []
+        state = []
+        for sec in self.sections:
+            state.append(np.array(sec.state))
+            x.append(sec.start)
+        x.append(sec.end)
+            
+        piecewise_poly = []
+        for i in range(self.n_entries):
+            c = np.array([s[i,:] for s in state])
+            c_decreasing = np.fliplr(c)
+            p = scipy.interpolate.PPoly(c_decreasing.T, x)
+            piecewise_poly.append(p)
+        c_decreasing = np.fliplr(state)
+        
+        self._piecewise_poly = piecewise_poly
+        
+
     @property
     def piecewise_poly(self):
         """list: scipy.interpolate.PPoly for each dimension.
@@ -253,26 +193,7 @@ class TimeLine():
             DESCRIPTION.
 
         """
-        x = []
-        state = []
-        for sec in self.sections:
-            state.append(np.array((sec.state),ndmin=2))
-            x.append(sec.start)
-        x.append(sec.end)
-            
-        piecewise_poly = []
-        for i in range(self.n_dim):
-            c = np.array([s[i,:] for s in state])
-            c_decreasing = np.fliplr(c)
-            p = scipy.interpolate.PPoly(c_decreasing.T, x)
-            piecewise_poly.append(p)
-        
-        return piecewise_poly
-    
-    @property
-    def n_dim(self):
-        if len(self.sections) > 0:
-            return self.sections[0].n_dim
+        return self._piecewise_poly
 
     def value(self, time):
         """np.array: Value of parameter at given time
@@ -283,7 +204,7 @@ class TimeLine():
             time points at which to evaluate.
 
         """
-        return np.array([p(time) for p in self.piecewise_poly], ndmin=2).T
+        return np.array([p(time) for p in self.piecewise_poly]).T
     
     def coefficients(self, time):
         """Return coefficient of polynomial at given time.
@@ -299,9 +220,9 @@ class TimeLine():
             !!! Array of coefficients in ORDER !!!
         """
         section_index = self.section_index(time)
-        c = self.sections[section_index].state
+        c = self.sections[section_index].state.copy()
         y = self.value(time)
-        c[:,0] = y[:,0]
+        c[:,0] = y
         
         return c
     
@@ -332,11 +253,9 @@ class TimeLine():
                 
         if not ((self.start <= start) & (start <= end) & (end <= self.end)):
             raise ValueError('Integration bounds exceed section times')
+
+        return np.array([p.integrate(start, end) for p in self.piecewise_poly]).T
         
-        integral = [p.integrate(start, end) for p in self.piecewise_poly]
-        
-        return np.array(integral)
-    
     def section_index(self, time):
         section_times = np.array(self.section_times)
         
@@ -354,10 +273,20 @@ class TimeLine():
     def end(self):
         return self.section_times[-1]
         
-    def plot(self):
+    def plot(self, ax=None, show=True):
+        if ax is None:
+            fig, ax = plt.subplots()        
         start = self.sections[0].start
         end = self.sections[-1].end
         time = np.linspace(start, end, 1001)
-        plt.figure()
-        plt.plot(time, self.value(time))
+        
+        ax.plot(time, self.value(time))
+        
+        ax.set_xlabel("time / s")
+        ax.set_ylabel("state")
+        
+        if show:
+            plt.show()
+            
+        return ax
 
--- tests/test_sections.py
+++ tests/test_sections.py
@@ -1,150 +1,234 @@
 import unittest
 
+import numpy as np
 from scipy import integrate
 
-class TestTimeLine(unittest.TestCase):
+from CADETProcess import CADETProcessError
+from CADETProcess.common import Section, TimeLine
+
+
+class TestSection(unittest.TestCase):
     def __init__(self, methodName='runTest'):
         super().__init__(methodName)
 
-    def create_time_line_1(self):
-        import CADETProcess
+    def create_constant_section_single(self):
+        return Section(0,1,1)
+    
+    def create_constant_section_multi(self):
+        return Section(1,2,[1,2])
+    
+    def create_poly_section_single(self):
+        return Section(0, 1, [0,1,0,0], n_entries=1, degree=3)
+    
+    def create_poly_section_multi(self):
+        return Section(0,1, [[0,1],[1,-1]], n_entries=2, degree=3)
+    
+    def test_section_value(self):
+        const_single = self.create_constant_section_single()
+        self.assertEqual(const_single.value(0), 1)
+        with self.assertRaises(ValueError):
+            # Exceed section times
+            val = const_single.value(2)
 
-        section_0 = CADETProcess.common.Section(0,1,(1.5,0))
-        section_1 = CADETProcess.common.Section(1,2,(0,0))
-        section_2 = CADETProcess.common.Section(2,3,(0,1))
-        section_3 = CADETProcess.common.Section(3,4,(1,0))
-        section_4 = CADETProcess.common.Section(4,5,(2,0))
-        section_5 = CADETProcess.common.Section(5,6,(2,-2))
+        const_multi = self.create_constant_section_multi()
+        np.testing.assert_equal(const_multi.value(1), [1,2])
+        with self.assertRaises(ValueError):
+            # Exceed section times
+            val = const_multi.value(0)
+        
+        poly_single = self.create_poly_section_single()
+        np.testing.assert_equal(poly_single.value(0), 0)
+        np.testing.assert_equal(poly_single.value(0.5), 0.5)
+        np.testing.assert_equal(poly_single.value(1), 1)
 
-        time_line_1 = CADETProcess.common.TimeLine()
-        time_line_1.add_section(section_0)
-        time_line_1.add_section(section_1)
-        time_line_1.add_section(section_2)
-        time_line_1.add_section(section_3)
-        time_line_1.add_section(section_4)
-        time_line_1.add_section(section_5)
+        poly_multi = self.create_poly_section_multi()
+        np.testing.assert_equal(poly_multi.value(0), [0,1])
+        np.testing.assert_equal(poly_multi.value(0.5), [0.5,0.5])
+        np.testing.assert_equal(poly_multi.value(1), [1,0])        
+        
+    def test_section_integral(self):
+        const_single = self.create_constant_section_single()
+        self.assertEqual(const_single.integral(0,0), 0)
+        self.assertEqual(const_single.integral(0,1), 1)
+        with self.assertRaises(ValueError):
+            # Exceed section times
+            val = const_single.value(2)
+        
+        const_multi = self.create_constant_section_multi()
+        np.testing.assert_equal(const_multi.integral(1,2), [1,2])
+        with self.assertRaises(ValueError):
+            # Exceed section times
+            val = const_multi.value(0)
+        
+        poly_single = self.create_poly_section_single()
+        np.testing.assert_equal(poly_single.integral(0, 0.5), 0.125)
+        np.testing.assert_equal(poly_single.integral(0, 1), 0.5)
+        
+        poly_multi = self.create_poly_section_multi()
+        np.testing.assert_equal(poly_multi.integral(0, 0.5), [0.125, 0.375])
+        np.testing.assert_equal(poly_multi.integral(0.5, 1), [0.375, 0.125])
+        np.testing.assert_equal(poly_multi.integral(0, 1), [0.5, 0.5])   
+        
+class TestTimeLine(unittest.TestCase):
+    def __init__(self, methodName='runTest'):
+        super().__init__(methodName)        
+        
+    def create_time_line_constant_single(self):
+        section_0 = Section(0,1,1.5)
+        section_1 = Section(1,2,0)
+        section_2 = Section(2,3,0)
+        section_3 = Section(3,4,1)
+        section_4 = Section(4,5,2)
+        section_5 = Section(5,6,2)
+    
+        tl = TimeLine()
+        tl.add_section(section_0)
+        tl.add_section(section_1)
+        tl.add_section(section_2)
+        tl.add_section(section_3)
+        tl.add_section(section_4)
+        tl.add_section(section_5)
+    
+        return tl
 
-        return time_line_1
+    def create_time_line_constant_multi(self):
+        section_0 = Section(0,1,(1.5,0))
+        section_1 = Section(1,2,(0,0))
+        section_2 = Section(2,3,(0,1))
+        section_3 = Section(3,4,(1,0))
+        section_4 = Section(4,5,(2,0))
+        section_5 = Section(5,6,(2,-2))
+    
+        tl = TimeLine()
+        tl.add_section(section_0)
+        tl.add_section(section_1)
+        tl.add_section(section_2)
+        tl.add_section(section_3)
+        tl.add_section(section_4)
+        tl.add_section(section_5)
+    
+        return tl    
+        
+    def create_time_line_poly_single(self):
+        section_0 = Section(0,1,(1.5,0), n_entries=1, degree=2)
+        section_1 = Section(1,2,(0,0), n_entries=1, degree=2)
+        section_2 = Section(2,3,(0,1), n_entries=1, degree=2)
+        section_3 = Section(3,4,(1,0), n_entries=1, degree=2)
+        section_4 = Section(4,5,(2,0), n_entries=1, degree=2)
+        section_5 = Section(5,6,(2,-2), n_entries=1, degree=2)
+        
+        tl = TimeLine()
+        tl.add_section(section_0)
+        tl.add_section(section_1)
+        tl.add_section(section_2)
+        tl.add_section(section_3)
+        tl.add_section(section_4)
+        tl.add_section(section_5)
+    
+        return tl
+    
+    def create_time_line_constant_multi(self):
+        section_0 = Section(0,1,(1.5,0))
+        section_1 = Section(1,2,(0,0))
+        section_2 = Section(2,3,(0,1))
+        section_3 = Section(3,4,(1,0))
+        section_4 = Section(4,5,(2,0))
+        section_5 = Section(5,6,(2,-2))
+    
+        tl = TimeLine()
+        tl.add_section(section_0)
+        tl.add_section(section_1)
+        tl.add_section(section_2)
+        tl.add_section(section_3)
+        tl.add_section(section_4)
+        tl.add_section(section_5)
+    
+        return tl    
+    
+    def create_time_line_poly_multi(self):
+        section_0 = Section(0,1, [[1.5, 0], [0, 1]], n_entries=2, degree=3)
+        section_1 = Section(1,2, [[0, 0], [1, 1]], n_entries=2, degree=3)
+        section_2 = Section(2,3, [[0, 1], [1, 1]], n_entries=2, degree=3)
+        section_3 = Section(3,4, [[1, 0], [0, 0]], n_entries=2, degree=3)
+        section_4 = Section(4,5, [[2, 0], [0, 0]], n_entries=2, degree=3)
+        section_5 = Section(5,6, [[2, -2], [0, 0, 1]], n_entries=2, degree=3)
+    
+        tl = TimeLine()
+        tl.add_section(section_0)
+        tl.add_section(section_1)
+        tl.add_section(section_2)
+        tl.add_section(section_3)
+        tl.add_section(section_4)
+        tl.add_section(section_5)
+    
+        return tl    
+    
+    def test_timeline_value(self):
+        tl = self.create_time_line_constant_single()
+        
+        np.testing.assert_equal(tl.value(0), 1.5)
+        np.testing.assert_equal(tl.value(1), 0.0)
+        np.testing.assert_equal(tl.value(2), 0.0)
+        np.testing.assert_equal(tl.value(3), 1.0)
+        np.testing.assert_equal(tl.value(4), 2.0)
+        np.testing.assert_equal(tl.value(5), 2.0)
+        np.testing.assert_equal(tl.value(5.5), 2.0)
+        np.testing.assert_equal(tl.value(6), 2.0)
+        
+        tl = self.create_time_line_constant_multi()
+    
+        np.testing.assert_equal(tl.value(0), [1.5, 0])
+        np.testing.assert_equal(tl.value(1), [0.0, 0])
+        np.testing.assert_equal(tl.value(2), [0.0, 1])
+        np.testing.assert_equal(tl.value(3), [1.0, 0])
+        np.testing.assert_equal(tl.value(4), [2.0, 0])
+        np.testing.assert_equal(tl.value(5), [2.0, -2])
+        np.testing.assert_equal(tl.value(5.5), [2.0, -2])
+        np.testing.assert_equal(tl.value(6), [2.0, -2])        
+    
+        tl = self.create_time_line_poly_single()
+        
+        np.testing.assert_equal(tl.value(2), 0.0)
+        np.testing.assert_equal(tl.value(2.5), 0.5)
+        np.testing.assert_equal(tl.value(5), 2.0)
+        np.testing.assert_equal(tl.value(5.5), 1.0)
+        np.testing.assert_equal(tl.value(6), 0.0)
+        
+        tl = self.create_time_line_poly_multi()
+        np.testing.assert_equal(tl.value(0), [1.5, 0])
+        np.testing.assert_equal(tl.value(2.5), [0.5, 1.5])
+        np.testing.assert_equal(tl.value(5), [2.0, 0])
+        np.testing.assert_equal(tl.value(5.5), [1.0, 0.25])
+        np.testing.assert_equal(tl.value(6), [0.0, 1])
+        
+    def test_timeline_integral(self):
+        tl = self.create_time_line_poly_single()
 
-    def test_time_line_1(self):
-        """Test concentration profile with steps and gradients"""
-        time_line_1 = self.create_time_line_1()
-        self.assertEqual(time_line_1.value(0), 1.5)
-        self.assertEqual(time_line_1.value(1), 0.0)
-        self.assertEqual(time_line_1.value(2), 0.0)
-        self.assertEqual(time_line_1.value(3), 1.0)
-        self.assertEqual(time_line_1.value(4), 2.0)
-        self.assertEqual(time_line_1.value(5), 2.0)
-        self.assertEqual(time_line_1.value(5.5), 1.0)
-        self.assertEqual(time_line_1.value(6), 0.0)
+        np.testing.assert_equal(tl.integral(0,0), 0.0)
+        np.testing.assert_equal(tl.integral(0,0.5), 1.5/2)
+        np.testing.assert_equal(tl.integral(0,1), 1.5)
+        np.testing.assert_equal(tl.integral(0,2), 1.5)
+        np.testing.assert_equal(tl.integral(2,2.5), 0.5/2/2)
+        np.testing.assert_equal(tl.integral(2,3), 0.5)
+        
+        tl = self.create_time_line_poly_multi()
+        
+        np.testing.assert_equal(tl.integral(0,0), [0.0, 0.0])
+        np.testing.assert_equal(tl.integral(0,0.5), [1.5/2, 0.125])
+        np.testing.assert_equal(tl.integral(0,1), [1.5, 0.5])
+        np.testing.assert_equal(tl.integral(0,2), [1.5, 2.0])
+        np.testing.assert_equal(tl.integral(5,6), [1, 1/3])
+        
+    def test_timeline_coeff(self):
+        tl = self.create_time_line_poly_multi()
+        
+        np.testing.assert_equal(
+            tl.coefficients(0.0), [[1.5, 0, 0, 0], [0, 1, 0, 0]]
+        )
 
-
-    def create_time_line_2_Q(self):
-        import CADETProcess
-
-        section_0 = CADETProcess.common.Section(0,1,1)
-        section_1 = CADETProcess.common.Section(1,2,0)
-
-        time_line_2_Q = CADETProcess.common.TimeLine()
-        time_line_2_Q.add_section(section_0)
-        time_line_2_Q.add_section(section_1)
-
-        return time_line_2_Q
-
-    def create_time_line_2_c(self):
-        import CADETProcess
-
-        section_0 = CADETProcess.common.Section(0,2,1)
-
-        time_line_2_c = CADETProcess.common.TimeLine()
-        time_line_2_c.add_section(section_0)
-
-        return time_line_2_c
-
-    def test_time_line_2(self):
-        """Feed situation: constant concentration and variable flow"""
-        time_line_2_Q = self.create_time_line_2_Q()
-        time_line_2_c = self.create_time_line_2_c()
-
-        integral = integrate.quad(
-            lambda t: time_line_2_Q.value(t) * time_line_2_c.value(t), 0,2
-        )[0]
-
-        self.assertEqual(integral, 1.0)
-
-
-    def create_time_line_3_Q(self):
-        import CADETProcess
-
-        section_0 = CADETProcess.common.Section(0,1,1)
-        section_1 = CADETProcess.common.Section(1,2,0)
-        section_2 = CADETProcess.common.Section(2,3,1)
-
-        time_line_3_Q = CADETProcess.common.TimeLine()
-        time_line_3_Q.add_section(section_0)
-        time_line_3_Q.add_section(section_1)
-        time_line_3_Q.add_section(section_2)
-
-        return time_line_3_Q
-
-    def create_time_line_3_c(self):
-        import CADETProcess
-
-        section_0 = CADETProcess.common.Section(0,0.5,0)
-        section_1 = CADETProcess.common.Section(0.5,2.5,1)
-        section_2 = CADETProcess.common.Section(2.5,3,0)
-
-        time_line_3_c = CADETProcess.common.TimeLine()
-        time_line_3_c.add_section(section_0)
-        time_line_3_c.add_section(section_1)
-        time_line_3_c.add_section(section_2)
-
-        return time_line_3_c
-
-    def test_time_line_3(self):
-        """Recycle situation: constant concentration with intermediate zero-flow
-        """
-        time_line_3_Q = self.create_time_line_3_Q()
-        time_line_3_c = self.create_time_line_3_c()
-
-        integral = integrate.quad(lambda t:
-        time_line_3_Q.value(t) * time_line_3_c.value(t), 0,3)[0]
-        self.assertAlmostEqual(integral, 1.0)
-
-
-    def create_time_line_4_Q(self):
-        import CADETProcess
-
-        section_0 = CADETProcess.common.Section(0,1,(0,1))
-        section_1 = CADETProcess.common.Section(1,2,0)
-
-        time_line_4_Q = CADETProcess.common.TimeLine()
-        time_line_4_Q.add_section(section_0)
-        time_line_4_Q.add_section(section_1)
-
-        return time_line_4_Q
-
-    def create_time_line_4_c(self):
-        import CADETProcess
-
-        section_0 = CADETProcess.common.Section(0,2,1)
-
-        time_line_4_c = CADETProcess.common.TimeLine()
-        time_line_4_c.add_section(section_0)
-
-        return time_line_4_c
-
-    def test_time_line_4(self):
-        """Gradient situation: constant concentration with variable flow rate.
-        """
-        time_line_4_Q = self.create_time_line_4_Q()
-        time_line_4_c = self.create_time_line_4_c()
-
-        integral = integrate.quad(lambda t:
-        time_line_4_Q.value(t) * time_line_4_c.value(t), 0,2)[0]
-
-        self.assertEqual(integral, 0.5)
-
+        np.testing.assert_equal(
+            tl.coefficients(5.5), [[1, -2, 0, 0], [0.25, 0, 1, 0]]
+        )
 
 if __name__ == '__main__':
     unittest.main()
