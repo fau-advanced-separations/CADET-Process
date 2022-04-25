d8c635b652d47e2b46bfb73dc506a2045369f8cb
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2022-04-25 17:47:35 +0200

    bump version to 0.6.0

--- CADETProcess/__init__.py
+++ CADETProcess/__init__.py
@@ -10,7 +10,7 @@ See https://cadet-process.readthedocs.io for complete documentation.
 """
 # Version information
 name = 'CADET-Process'
-__version__ = '0.5.2'
+__version__ = '0.6.0'
 
 # Imports
 from .CADETProcessError import *
