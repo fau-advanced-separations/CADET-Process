b93e86ce09d4890c64a4206206ca08afd2e7ab7a
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2022-06-24 15:31:48 +0200

    Improve parameter transform.
    - [x] Better distinguish between input and output bounds.
    - [x] Add flags to allow exceeding input and output range.
    - [x] Add unit tests.

--- CADETProcess/transform.py
+++ CADETProcess/transform.py
@@ -4,9 +4,54 @@ import numpy as np
 
 
 class TransformBase(ABC):
-    def __init__(self, lb=-np.inf, ub=np.inf):
-        self._lb = lb
-        self._ub = ub
+    """Base Class for parameter transformation.
+
+    Maps input parameter space to some output parameter space.
+
+    Attributes
+    ----------
+    lb_input : {float, array}
+        Lower bounds of input parameter space.
+    ub_input : {float, array}
+        Upper bounds of input parameter space.
+    lb: {float, array}
+        Lower bounds of output parameter space.
+    ub : {float, array}
+        Upper bounds of output parameter space.
+    allow_extended_input : bool
+        If True, the input value may exceed the lower/upper bounds.
+        Else, an exception is thrown.
+        The default is False.
+    allow_extended_output : bool
+        If True, the output value may exceed the lower/upper bounds.
+        Else, an exception is thrown.
+        The default is False.
+    """
+
+    def __init__(
+            self,
+            lb_input=-np.inf, ub_input=np.inf,
+            allow_extended_input=False, allow_extended_output=False):
+        self.lb_input = lb_input
+        self.ub_input = ub_input
+        self.allow_extended_input = allow_extended_input
+        self.allow_extended_output = allow_extended_output
+
+    @property
+    def lb_input(self):
+        return self._lb_input
+
+    @lb_input.setter
+    def lb_input(self, lb_input):
+        self._lb_input = lb_input
+
+    @property
+    def ub_input(self):
+        return self._ub_input
+
+    @ub_input.setter
+    def ub_input(self, ub_input):
+        self._ub_input = ub_input
 
     @abstractproperty
     def lb(self):
@@ -16,12 +61,38 @@ class TransformBase(ABC):
     def ub(self):
         pass
 
-    @abstractmethod
     def transform(self, x):
-        pass
+        if (
+                not self.allow_extended_input and
+                not np.all((self.lb_input <= x) * (x <= self.ub_input))):
+            raise ValueError("Value exceeds input bounds.")
+        x = self._transform(x)
+        if (
+                not self.allow_extended_output and
+                not np.all((self.lb <= x) * (x <= self.ub))):
+            raise ValueError("Value exceeds output bounds.")
+
+        return x
 
     @abstractmethod
+    def _transform(self, x):
+        pass
+
     def untransform(self, x):
+        if (
+                not self.allow_extended_output and
+                not np.all((self.lb <= x) * (x <= self.ub))):
+            raise ValueError("Value exceeds output bounds.")
+        x = self._untransform(x)
+        if (
+                not self.allow_extended_input and
+                not np.all((self.lb_input <= x) * (x <= self.ub_input))):
+            raise ValueError("Value exceeds input bounds.")
+
+        return x
+
+    @abstractmethod
+    def _untransform(self, x):
         pass
 
     def __str__(self):
@@ -31,131 +102,125 @@ class TransformBase(ABC):
 class NoTransform(TransformBase):
     @property
     def lb(self):
-        return self._lb
-
-    @lb.setter
-    def lb(self, lb):
-        self._lb = lb
+        return self.lb_input
 
     @property
     def ub(self):
-        return self._ub
+        return self.ub_input
 
-    @ub.setter
-    def ub(self, ub):
-        self._ub = ub
-
-    def transform(self, x):
+    def _transform(self, x):
         return x
 
-    def untransform(self, x):
+    def _untransform(self, x):
         return x
-
-
-class AutoTransform(TransformBase):
-    @property
-    def lb(self):
-        return 0
-
-    @lb.setter
-    def lb(self, lb):
-        self.linear.lb = lb
-        self.log.lb = lb
-        self._lb = lb
-
-    @property
-    def ub(self):
-        return 1
-
-    @ub.setter
-    def ub(self, ub):
-        self.linear.ub = ub
-        self.log.ub = ub
-        self._ub = ub
-
-    def __init__(self, lb=-np.inf, ub=np.inf, threshold=1000):
-        self.linear = NormLinearTransform(lb, ub)
-        self.log = NormLogTransform(lb, ub)
-        self.max_factor = 1000
-        self.lb = lb
-        self.ub = ub
-
-    @property
-    def use_linear(self):
-        if self._lb <= 0:
-            return np.log10(self._ub - self._lb) <= np.log10(self.max_factor)
-        else:
-            return self._ub/self._lb <= self.max_factor
-
-    @property
-    def use_log(self):
-        return not self.use_linear
-
-    def transform(self, x):
-        if self.use_log:
-            return self.log.transform(x)
-        else:
-            return self.linear.transform(x)
-
-    def untransform(self, x):
-        if self.use_log:
-            return self.log.untransform(x)
-        else:
-            return self.linear.untransform(x)
 
 
 class NormLinearTransform(TransformBase):
     @property
     def lb(self):
-        return 1
-
-    @lb.setter
-    def lb(self, lb):
-        self._lb = lb
+        return 0
 
     @property
     def ub(self):
-        return 0
+        return 1
 
-    @ub.setter
-    def ub(self, ub):
-        self._ub = ub
+    def _transform(self, x):
+        return (x - self.lb_input) / (self.ub_input - self.lb_input)
 
-    def transform(self, x):
-        return (x - self._lb) / (self._ub - self._lb)
-
-    def untransform(self, x):
-        return (self._ub - self._lb) * x + self._lb
+    def _untransform(self, x):
+        return (self.ub_input - self.lb_input) * x + self.lb_input
 
 
 class NormLogTransform(TransformBase):
     @property
     def lb(self):
-        return 1
-
-    @lb.setter
-    def lb(self, lb):
-        self._lb = lb
+        return 0
 
     @property
     def ub(self):
+        return 1
+
+    def _transform(self, x):
+        if self.lb_input <= 0:
+            x_ = x + (abs(self.lb_input) + 1)
+            ub = 1 + (self.ub_input - self.lb_input)
+            return np.log(x_) / (np.log(ub))
+
+        else:
+            return \
+                np.log(x/self.lb_input) / np.log(self.ub_input/self.lb_input)
+
+    def _untransform(self, x):
+        if self.lb_input < 0:
+            return \
+                np.exp(x * np.log(self._ub - self.lb_input + 1)) \
+                + self.lb_input - 1
+        else:
+            return \
+                self.lb_input * np.exp(x * np.log(self.ub_input/self.lb_input))
+
+
+class AutoTransform(TransformBase):
+    def __init__(self, *args, threshold=1000, **kwargs):
+        self.linear = NormLinearTransform()
+        self.log = NormLogTransform()
+
+        super().__init__(*args, **kwargs)
+        self.threshold = threshold
+        self.linear.allow_extended_input = self.allow_extended_input
+        self.linear.allow_extended_output = self.allow_extended_input
+        self.log.allow_extended_input = self.allow_extended_input
+        self.log.allow_extended_output = self.allow_extended_input
+
+    @property
+    def use_linear(self):
+        if self.lb_input <= 0:
+            return \
+                np.log10(self.ub_input - self.lb_input) \
+                <= np.log10(self.threshold)
+        else:
+            return self.ub_input/self.lb_input <= self.threshold
+
+    @property
+    def use_log(self):
+        return not self.use_linear
+
+    @property
+    def lb(self):
         return 0
 
-    @ub.setter
-    def ub(self, ub):
-        self._ub = ub
+    @property
+    def ub(self):
+        return 1
 
-    def transform(self, x):
-        if self.lb <= 0:
-            x_ = x + (abs(self._lb) + 1)
-            ub_ = 1 + (self._ub - self._lb)
-            return np.log(x_) / (np.log(ub_))
+    @property
+    def lb_input(self):
+        return self._lb_input
 
+    @lb_input.setter
+    def lb_input(self, lb_input):
+        self.linear.lb_input = lb_input
+        self.log.lb_input = lb_input
+        self._lb_input = lb_input
+
+    @property
+    def ub_input(self):
+        return self._ub_input
+
+    @ub_input.setter
+    def ub_input(self, ub_input):
+        self.linear.ub_input = ub_input
+        self.log.ub_input = ub_input
+        self._ub_input = ub_input
+
+    def _transform(self, x):
+        if self.use_log:
+            return self.log.transform(x)
         else:
-            return np.log(x/self._lb) / np.log(self._ub/self._lb)
+            return self.linear.transform(x)
 
-    def untransform(self, x):
-        if self.lb < 0:
-            return np.exp(x * np.log(self._ub - self._lb + 1)) + self._lb - 1
+    def _untransform(self, x):
+        if self.use_log:
+            return self.log.untransform(x)
         else:
-            return self._lb * np.exp(x * np.log(self._ub/self._lb))
+            return self.linear.untransform(x)
---
+++ tests/test_transform.py
@@ -0,0 +1,144 @@
+import unittest
+
+from CADETProcess.transform import (
+    NoTransform, NormLinearTransform, NormLogTransform, AutoTransform
+)
+
+
+class Test_Transform(unittest.TestCase):
+    def __init__(self, methodName='runTest'):
+        super().__init__(methodName)
+
+    def test_input_range(self):
+        transform = NormLinearTransform(0, 100)
+
+        with self.assertRaises(ValueError):
+            in_ = -10
+            out = transform.transform(in_)
+
+        with self.assertRaises(ValueError):
+            in_ = 1000
+            out = transform.transform(in_)
+
+    def test_output_range(self):
+        transform = NormLinearTransform(0, 100)
+
+        with self.assertRaises(ValueError):
+            in_ = -1
+            out = transform.untransform(in_)
+
+        with self.assertRaises(ValueError):
+            in_ = 2
+            out = transform.untransform(in_)
+
+    def test_no_transform(self):
+        transform = NoTransform(0, 100)
+        self.assertAlmostEqual(transform.lb, 0)
+        self.assertAlmostEqual(transform.ub, 100)
+
+        in_ = 0
+        out_expected = 0
+        out = transform.transform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 0
+        out_expected = 0
+        out = transform.untransform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+    def test_linear(self):
+        transform = NormLinearTransform(0, 100)
+        self.assertAlmostEqual(transform.lb, 0)
+        self.assertAlmostEqual(transform.ub, 1)
+
+        in_ = 0
+        out_expected = 0
+        out = transform.transform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 0
+        out_expected = 0
+        out = transform.untransform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 10
+        out_expected = 0.1
+        out = transform.transform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 0.1
+        out_expected = 10
+        out = transform.untransform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 100
+        out_expected = 1
+        out = transform.transform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 1
+        out_expected = 100
+        out = transform.untransform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+    def test_log(self):
+        """Missing: Special case for lb_input <= 0"""
+        transform = NormLogTransform(1, 1000)
+        self.assertAlmostEqual(transform.lb, 0)
+        self.assertAlmostEqual(transform.ub, 1)
+
+        in_ = 1
+        out_expected = 0
+        out = transform.transform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 0
+        out_expected = 1
+        out = transform.untransform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 10
+        out_expected = 1/3
+        out = transform.transform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 1/3
+        out_expected = 10
+        out = transform.untransform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 100
+        out_expected = 2/3
+        out = transform.transform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 2/3
+        out_expected = 100
+        out = transform.untransform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 1000
+        out_expected = 1
+        out = transform.transform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+        in_ = 1
+        out_expected = 1000
+        out = transform.untransform(in_)
+        self.assertAlmostEqual(out_expected, out)
+
+    def test_auto(self):
+        threshold = 1000
+
+        transform = AutoTransform(1, 100, threshold=threshold)
+        self.assertTrue(transform.use_linear)
+        self.assertFalse(transform.use_log)
+
+        # Expect Log behaviour
+        transform = AutoTransform(1, 1001, threshold=threshold)
+        self.assertFalse(transform.use_linear)
+        self.assertTrue(transform.use_log)
+
+
+if __name__ == '__main__':
+    unittest.main()
