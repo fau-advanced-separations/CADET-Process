2e5f1b11570d79ae2813664836a1209e2a4472c0
flo-schu <fluncki@protonmail.com>
2023-07-19 19:05:47 +0200

    add evaluator to constraints to reproduce error in jos problem

--- tests/optimization_problem_fixtures.py
+++ tests/optimization_problem_fixtures.py
@@ -378,8 +378,17 @@ class NonlinearConstraintsMooTestProblem(TestProblem):
 
         self.add_variable('var_0', lb=-20, ub=20)
         self.add_variable('var_1', lb=-20, ub=20)
-        self.add_nonlinear_constraint(self._nonlincon_fun, n_nonlinear_constraints=2)
+        self.setup_nonlinear_constraints(has_evaluator=has_evaluator)
         self.setup_objectives(has_evaluator=has_evaluator)
+
+    def setup_nonlinear_constraints(self, has_evaluator):
+        if has_evaluator:
+            self.add_evaluator(self._nonlincon_fun)
+        self.add_nonlinear_constraint(
+            nonlincon=lambda res: res,
+            requires=[self._nonlincon_fun] if has_evaluator else None,
+            n_nonlinear_constraints=2
+        )
 
     def setup_objectives(self, has_evaluator):
         if has_evaluator:
