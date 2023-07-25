fix dimensionality problem for scaling, implement convergence test

CADETProcess/optimization/surrogate.py
@@ -173,6 +173,7 @@ class Surrogate:
         X_ = np.array(X, ndmin=2)
         X_scaled = self.X_scaler.transform(X_)
         F_mean_est_scaled = self.surrogate_model_F.predict(X_scaled)
+        F_mean_est_scaled = np.array(F_mean_est_scaled, ndmin=2)
         F_mean_est = self.F_scaler.inverse_transform(F_mean_est_scaled)
         # always cast as multi objective problem
         F_mean_est = F_mean_est.reshape((len(X_), -1))
@@ -225,6 +226,7 @@ class Surrogate:
         X_scaled = self.X_scaler.transform(X_)
 
         G_est_scaled = self.surrogate_model_G.predict(X_scaled)
+        G_est_scaled = np.array(G_est_scaled, ndmin=2)
         G_est = self.G_scaler.inverse_transform(G_est_scaled)
         G_est = G_est.reshape((len(X_), -1))
 
@@ -238,6 +240,7 @@ class Surrogate:
         X_scaled = self.X_scaler.transform(X_)
 
         CV_est_scaled = self.surrogate_model_CV.predict(X_scaled)
+        CV_est_scaled = np.array(CV_est_scaled, ndmin=2)
         CV_est = self.CV_scaler.inverse_transform(CV_est_scaled)
 
         CV_est = CV_est.reshape((len(X_), -1))
@@ -958,20 +961,20 @@ class Surrogate:
                     x0 = chebyshev_orig
 
                 try:
-                    # if len(x0) == op.n_variables:
-                    x_free = self.optimize_conditioned_problem(
-                        optimization_problem=op,
-                        x0=x0,
-                    )
-                    # else:
-                    #     warnings.warn(f"hopsy generated wrong x0={x0}.")
-                    #     problem = op.create_hopsy_problem(simplify=False)
-                    #     x0 = hopsy.compute_chebyshev_center(problem)[:, 0]
+                    if len(x0) == op.n_variables:
+                        x_free = self.optimize_conditioned_problem(
+                            optimization_problem=op,
+                            x0=x0,
+                        )
+                    else:
+                        warnings.warn(f"hopsy generated wrong x0={x0}.")
+                        problem = op.create_hopsy_problem(simplify=False)
+                        x0 = hopsy.compute_chebyshev_center(problem)[:, 0]
 
-                    #     x_free = self.optimize_conditioned_problem(
-                    #         optimization_problem=op,
-                    #         x0=x0,
-                    #     )
+                        x_free = self.optimize_conditioned_problem(
+                            optimization_problem=op,
+                            x0=x0,
+                        )
                 except CADETProcessError:
                     x_free = self.optimize_conditioned_problem(
                         optimization_problem=op,
@@ -1141,7 +1144,12 @@ class Surrogate:
         n_objectives = self.optimization_problem.n_objectives
 
         if axes is None:
-            _, axes = plt.subplots(n_objectives)
+            fig, axes = plt.subplots(n_objectives)
+            if isinstance(axes, plt.Axes):
+                axes = [axes]
+            self_contained_figure = True
+        else:
+            self_contained_figure = False
 
         for oi, ax in enumerate(axes):
             ax.scatter(x, f[:, oi], s=10, label="obj. fun", alpha=alpha, color=color)
@@ -1162,6 +1170,9 @@ class Surrogate:
             #         F_mean[:, oi]-F_std[:, oi], F_mean[:, oi]+F_std[:, oi],
             #         color="red", alpha=.5
             #     )
+
+        if self_contained_figure == True:
+            fig.show()
 
         return x_opt, f_min
 

tests/optimization_problem_fixtures.py
@@ -4,8 +4,10 @@ TODO:
 - [ ] Add documentation / scope of tests (e.g. link to scipy/pymoo)
 
 """
+import warnings
 
 import numpy as np
+
 from CADETProcess.optimization import OptimizationProblem, OptimizationResults
 from CADETProcess.transform import NormLinearTransform, NormLogTransform
 
@@ -29,6 +31,26 @@ class TestProblem(OptimizationProblem):
     @property
     def x0(self):
         raise NotImplementedError
+
+
+    @property
+    def conditional_minima(self):
+        warnings.warn("No known conditional minima implemented.")
+        return None
+
+    def test_points_on_conditional_minimum(self, X, F, var_index):
+        F_min_x = self.conditional_minima
+        if F_min_x is None:
+            return
+        else:
+            F_min_x = F_min_x[var_index]
+
+        for x, f in zip(X, F):
+            if np.all(np.isnan(x)):
+                continue
+            for j in range(self.n_objectives):
+                f_min_true = F_min_x(x[j, var_index])
+            np.testing.assert_allclose(f_min_true, f, rtol=0.05, atol=0.01)
 
 
 class Rosenbrock(TestProblem):
@@ -111,6 +133,26 @@ class LinearConstraintsSooTestProblem(TestProblem):
         f = -3
 
         return x, f
+
+    @property
+    def conditional_minima(self):
+        f_x0 = lambda x0:  x0 - 2
+        f_x1 = lambda x1:  x1 * - 3/2
+        return f_x0, f_x1
+
+    def test_points_on_conditional_minimum(self, X, F, var_index):
+        F_min_x = self.conditional_minima[var_index]
+        for x, f in zip(X, F):
+            if np.all(np.isnan(x)):
+                continue
+            for j in range(self.n_objectives):
+                # calculates the minimum with respect to a (fixed)
+                # conditioning value
+                f_min_true = F_min_x(x[j, var_index])
+                # test if this is equal to the value determined by the
+                # local optimizer executed in the surrogate.find_minimum
+                # method
+                np.testing.assert_allclose(f, f_min_true, rtol=0.1, atol=0.1)
 
     def test_if_solved(self, optimization_results: OptimizationResults, decimal=7):
         x_true, f_true = self.optimal_solution()
@@ -320,8 +362,7 @@ class LinearConstraintsMooTestProblem(TestProblem):
 
         self.add_linear_constraint(['var_0', 'var_1'], [-1, -1], -3)
         self.add_linear_constraint(['var_0', 'var_1'], [ 1, -1],  5)
-
-        self.add_objective(self._objective_function, n_objectives=2)
+        self.setup_objectives()
 
     @staticmethod
     def _objective_function(x):
@@ -329,6 +370,16 @@ class LinearConstraintsMooTestProblem(TestProblem):
         f2 = (1 + x[1]) / x[0]
 
         return f1, f2
+
+    def setup_objectives(self):
+        def f1(x):
+            return self._objective_function(x)[0]
+
+        def f2(x):
+            return self._objective_function(x)[1]
+
+        self.add_objective(f1, n_objectives=1)
+        self.add_objective(f2, n_objectives=1)
 
     def find_corresponding_x2(self, x1):
         """
@@ -354,7 +405,6 @@ class LinearConstraintsMooTestProblem(TestProblem):
         x2_test = np.where(x1 <= 3, 3 - x1, 0)
 
         np.testing.assert_almost_equal(x2, x2_test, decimal=decimal)
-
 
 
 class LinearNonlinearConstraintsMooTestProblem(TestProblem):