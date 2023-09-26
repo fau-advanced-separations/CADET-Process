dd317a16a06e7266be8c3e50b60c276c55c16229
r.jaepel <r.jaepel@fz-juelich.de>
2023-09-26 14:20:05 +0200

    Fix missing prop_cycler error

--- CADETProcess/solution.py
+++ CADETProcess/solution.py
@@ -1446,9 +1446,10 @@ def _plot_solution_1D(
     y_max = 0
     y_min_sec = 0
     y_max_sec = 0
+    colors = iter(plt.rcParams["axes.prop_cycle"].by_key()["color"])
 
     for i, comp in enumerate(solution.component_system.components):
-        color = next(ax._get_lines.prop_cycler)['color']
+        color = next(colors)
         if hide_labels or not update_layout:
             label = None
         else:
