bb4f62c9576fb9f6c3e4a81fabd75bf103afc3ce
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2024-06-26 16:00:31 +0200

    Do not plot figures if progress_frequency is None
    Plotting figures creates a major overhead during optimization. With this
    option set to `None` by default, only the final results are plotted.

--- tests/test_optimizer_behavior.py
+++ tests/test_optimizer_behavior.py
@@ -145,7 +145,9 @@ def optimization_problem(request):
     qNParEGO
 ])
 def optimizer(request):
-    return request.param()
+    optimizer = request.param()
+    optimizer.progress_freqency = None
+    return optimizer
 
 
 # %% Tests
