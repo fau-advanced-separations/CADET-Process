68cbd675384f0fdd6a570393612db2566e4187df
Florian <33749653+flo-schu@users.noreply.github.com>
2024-11-19 16:07:47 +0100

    Use SingleTaskGP instead of FixedNoiseGP as recommended in the Deprecation Warning
    Co-authored-by: r.jaepel <ronald.jaepel@gmail.com>

--- CADETProcess/optimization/axAdapater.py
+++ CADETProcess/optimization/axAdapater.py
@@ -20,7 +20,7 @@ from ax.models.torch.botorch_modular.surrogate import Surrogate
 from ax.utils.common.result import Err, Ok
 from ax.service.utils.report_utils import exp_to_df
 from botorch.utils.sampling import manual_seed
-from botorch.models.gp_regression import FixedNoiseGP
+from botorch.models.gp_regression import SingleTaskGP
 from botorch.acquisition.analytic import (
     LogExpectedImprovement
 )
@@ -521,7 +521,7 @@ class BotorchModular(SingleObjectiveAxInterface):
     surrogate_model: Model class
     """
     acquisition_fn = Typed(ty=type, default=LogExpectedImprovement)
-    surrogate_model = Typed(ty=type, default=FixedNoiseGP)
+    surrogate_model = Typed(ty=type, default=SingleTaskGP)
 
     _specific_options = [
         'acquisition_fn', 'surrogate_model'
@@ -552,7 +552,7 @@ class NEHVI(MultiObjectiveAxInterface):
     supports_single_objective = False
 
     def __repr__(self):
-        smn = 'FixedNoiseGP'
+        smn = 'SingleTaskGP'
         afn = 'NEHVI'
 
         return f'{smn}+{afn}'
@@ -578,7 +578,7 @@ class qNParEGO(MultiObjectiveAxInterface):
     supports_single_objective = False
 
     def __repr__(self):
-        smn = 'FixedNoiseGP'
+        smn = 'SingleTaskGP'
         afn = 'qNParEGO'
 
         return f'{smn}+{afn}'
