ab054df0d0e0b8754e9c9c358e2792856106dda8
r.jaepel <ronald.jaepel@gmail.com>
2025-02-19 10:14:20 +0100

    Add function optimizer.load_results() to load and return OptimizationResults based on a `checkpoint.h5` or `final.h5`

--- tests/test_optimization_results.py
+++ tests/test_optimization_results.py
@@ -1,4 +1,6 @@
+import shutil
 import unittest
+from pathlib import Path
 
 import numpy as np
 
@@ -9,13 +11,19 @@ from tests.test_population import setup_population
 from tests.test_optimization_problem import setup_optimization_problem
 
 
-def setup_optimization_results(
+def setup_optimization_problem_and_results(
         n_gen=3, n_ind=3, n_vars=2, n_obj=1, n_nonlin=0, n_meta=0, rng=None,
         initialize_data=True):
     optimization_problem = setup_optimization_problem(n_vars, n_obj, n_nonlin, n_meta)
     optimizer = U_NSGA3()
 
     optimization_results = OptimizationResults(optimization_problem, optimizer)
+    results_dir = Path("tmp") / "optimization_results"
+
+    shutil.rmtree(results_dir, ignore_errors=True)
+    results_dir.mkdir(exist_ok=True, parents=True)
+    (results_dir / "figures").mkdir(exist_ok=True, parents=True)
+    optimization_results.results_directory = results_dir
 
     if initialize_data:
         if rng is None:
@@ -28,12 +36,12 @@ def setup_optimization_results(
             if n_meta > 0:
                 optimization_results.update_meta()
 
-    return optimization_results
+    return optimization_problem, optimization_results
 
 
 class TestOptimizationResults(unittest.TestCase):
     def setUp(self):
-        self.optimization_results = setup_optimization_results()
+        _, self.optimization_results = setup_optimization_problem_and_results()
 
     def test_history(self):
         n_gen_expected = 3
@@ -63,10 +71,24 @@ class TestOptimizationResults(unittest.TestCase):
     def test_serialization(self):
         data = self.optimization_results.to_dict()
 
-        results_new = setup_optimization_results(initialize_data=False)
+        _, results_new = setup_optimization_problem_and_results(initialize_data=False)
         results_new.update_from_dict(data)
         data_new = results_new.to_dict()
         np.testing.assert_equal(data, data_new)
+
+    def test_io_serialization(self):
+        optimization_problem, _ = setup_optimization_problem_and_results()
+
+        # save_results() needs to happen after creation of optimization_problem,
+        #  because during optimization_problem creation the results folder gets cleared and that would delete the save
+        self.optimization_results.save_results("checkpoint")
+
+        optimizer = U_NSGA3()
+        optimization_results_new = optimizer.load_results(
+            checkpoint_path=self.optimization_results.results_directory / "checkpoint.h5",
+            optimization_problem=optimization_problem
+        )
+        np.testing.assert_equal(self.optimization_results.to_dict(), optimization_results_new.to_dict())
 
 
 if __name__ == '__main__':
