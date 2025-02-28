1dc1725673c7773eed536c391e89b8cac104ae93
r.jaepel <ronald.jaepel@gmail.com>
2025-02-28 11:38:45 +0100

    v0.10.1

--- CADETProcess/__init__.py
+++ CADETProcess/__init__.py
@@ -10,7 +10,7 @@ See https://cadet-process.readthedocs.io for complete documentation.
 """
 # Version information
 name = "CADET-Process"
-__version__ = "0.10.0"
+__version__ = "0.10.1"
 
 # Imports
 from .CADETProcessError import *
