fa20c81cf9e9caf3009e455ea744c671f72d6906
Hannah Lanzrath <h.lanzrath@fz-juelich.de>
2026-01-28 13:46:45 +0100

    Catch import errors in MacOS Arm CI runners

--- CADETProcess/sysinfo.py
+++ CADETProcess/sysinfo.py
@@ -1,20 +1,37 @@
 import platform
-
 import psutil
 
 uname = platform.uname()
-cpu_freq = psutil.cpu_freq()
-memory = psutil.virtual_memory()
-memory_total = psutil._common.bytes2human(memory.total)
 
 system_information = {
     "system": uname.system,
     "release": uname.release,
     "machine": uname.machine,
     "processor": uname.processor,
-    "n_cores": psutil.cpu_count(logical=True),
-    "n_cores_physical": psutil.cpu_count(logical=False),
-    "max_frequency": cpu_freq.max,
-    "min_frequency": cpu_freq.min,
-    "memory_total": memory_total,
+    "n_cores": None,
+    "n_cores_physical": None,
+    "max_frequency": None,
+    "min_frequency": None,
+    "memory_total": None,
 }
+
+if psutil is not None:
+    try:
+        system_information["n_cores"] = psutil.cpu_count(logical=True)
+        system_information["n_cores_physical"] = psutil.cpu_count(logical=False)
+    except Exception:
+        pass
+
+    try:
+        cpu_freq = psutil.cpu_freq()
+        if cpu_freq is not None:
+            system_information["max_frequency"] = cpu_freq.max
+            system_information["min_frequency"] = cpu_freq.min
+    except Exception:
+        pass
+
+    try:
+        memory = psutil.virtual_memory()
+        system_information["memory_total"] = f"{memory.total / 1024**3:.1f} GiB"
+    except Exception:
+        pass
