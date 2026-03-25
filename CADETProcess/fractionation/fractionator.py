4c0a8cae8f5c52b5937cdc75a0b9ccab5b3219d8
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2026-03-25 13:40:19 +0100

    Fix fractionation plot with secondary axes

--- CADETProcess/fractionation/fractionator.py
+++ CADETProcess/fractionation/fractionator.py
@@ -289,7 +289,8 @@ class Fractionator(EventHandler):
             **kwargs,
         )
 
-        y_max = 1.1 * np.max(chromatogram.solution)
+        primary_ax = fig.get_axes()[0]
+        y_max = primary_ax.get_ylim()[1]
 
         self._fill_fraction_overlay(
             ax,
