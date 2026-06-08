1a1433e09f4eb24a07b00a69acba51b151270409
Johannes Schmölder <j.schmoelder@fz-juelich.de>
2026-06-08 14:34:40 +0200

    Use chromatogram factory in comparison tests
    The factory needed t_end, n_time, and heights parameters to cover the 0–100 time range and mixed-height signals in the comparison suite.

--- tests/chromatogram_factory.py
+++ tests/chromatogram_factory.py
@@ -5,7 +5,15 @@ from CADETProcess.processModel import ComponentSystem
 from CADETProcess.solution import SolutionIO
 
 
-def rectangle_chromatogram(component_system: ComponentSystem, windows, name="outlet"):
+def rectangle_chromatogram(
+    component_system: ComponentSystem,
+    windows,
+    heights=None,
+    t_start=0.0,
+    t_end=10.0,
+    n_time=1001,
+    name="outlet",
+):
     """Rectangular pulse chromatogram.
 
     Parameters
@@ -14,30 +22,51 @@ def rectangle_chromatogram(component_system: ComponentSystem, windows, name="out
     windows : list of list of (start, end), one inner list per component
         e.g. [[(2, 4), (7, 9)]] for one component with two peaks,
         or [[(2, 4)], [(7, 9)]] for two separated components.
+    heights : list of float, optional
+        Peak height per component. Defaults to 1.0 for all components.
+    t_start : float, optional
+        Start of the time axis. Default is 0.0.
+    t_end : float, optional
+        End of the time axis. Default is 10.0.
+    n_time : int, optional
+        Number of time points. Default is 1001.
     name : str, optional
         Name of the SolutionIO object. Default is "outlet".
     """
-    n_time = 1001
-    time = np.linspace(0, 10, n_time)
+    if heights is None:
+        heights = [1.0] * component_system.n_comp
+    time = np.linspace(t_start, t_end, n_time)
     solution = np.zeros((n_time, component_system.n_comp))
-    for i, comp_windows in enumerate(windows):
+    for i, (comp_windows, h) in enumerate(zip(windows, heights)):
         for t0, t1 in comp_windows:
-            solution[(time >= t0) & (time < t1), i] = 1.0
+            solution[(time >= t0) & (time < t1), i] = h
     return SolutionIO(name, component_system, time, solution, np.ones(n_time))
 
 
-def gaussian_chromatogram(component_system: ComponentSystem, peaks, name="outlet"):
+def gaussian_chromatogram(
+    component_system: ComponentSystem,
+    peaks,
+    t_start=0.0,
+    t_end=10.0,
+    n_time=1001,
+    name="outlet",
+):
     """Gaussian pulse chromatogram.
 
     Parameters
     ----------
     component_system : ComponentSystem
     peaks : list of (mu, sigma) per component
+    t_start : float, optional
+        Start of the time axis. Default is 0.0.
+    t_end : float, optional
+        End of the time axis. Default is 10.0.
+    n_time : int, optional
+        Number of time points. Default is 1001.
     name : str, optional
         Name of the SolutionIO object. Default is "outlet".
     """
-    n_time = 1001
-    time = np.linspace(0, 10, n_time)
+    time = np.linspace(t_start, t_end, n_time)
     solution = np.zeros((n_time, component_system.n_comp))
     for i, (mu, sigma) in enumerate(peaks):
         solution[:, i] = np.exp(-0.5 * ((time - mu) / sigma) ** 2) / (sigma * np.sqrt(2 * np.pi))
