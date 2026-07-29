f543d6040ff006ae599679a918a0103f1a196298
Hannah Lanzrath <h.lanzrath@fz-juelich.de>
2026-07-29 12:33:34 +0200

    Add least squares

--- CADETProcess/optimization/__init__.py
+++ CADETProcess/optimization/__init__.py
@@ -40,6 +40,7 @@ Scipy
    NelderMead
    SLSQP
    LBFGSB
+   LeastSquares
 
 IPOPT
 -----
@@ -111,7 +112,7 @@ from .results import *
 from .optimization_problem import OptimizationProblem
 from .parallelizationBackend import *
 from .optimizer import *
-from .scipyAdapter import COBYLA, COBYQA, TrustConstr, NelderMead, SLSQP, LBFGSB
+from .scipyAdapter import COBYLA, COBYQA, TrustConstr, NelderMead, SLSQP, LBFGSB, LeastSquares
 from .pymooAdapter import NSGA2, U_NSGA3
 
 import importlib
